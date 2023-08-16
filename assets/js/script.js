8b7e2f3dfde182fcfb659b3fb3f17d9ff96519a2
A.Yassine <ayassine.auce@gmail.com>
2023-08-16 21:57:46 +0300

    clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,21 @@
-// let playerName = "ahmad"
-// let chips = 200
 let player = { //shorthand
     name:"ahmad",
     chips:200
 }
 let isAlive = false
-// let cards = []
 let sum = 0
 const messageEl = document.getElementById("message-el")
 const cardsEl = document.getElementById("cards-el")
 const sumEl = document.getElementById("sum-el")
 const playerEl = document.getElementById("player-el")
-
 messageEl.innerHTML = "Want to play a round?"
 cardsEl.innerHTML = "Cards: "
 sumEl.innerHTML = "Sum: "
 playerEl.innerHTML = `${player.name}: $${player.chips}`
-
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() *13)+1
     return randomNumber
 }
-
 function startGame() {
     if(isAlive == false){
         isAlive = true
@@ -33,7 +27,6 @@ function startGame() {
         sumEl.innerHTML = `Sum: ${sum}`
         playerEl.innerHTML = `${player.name}: $${player.chips}`
         renderGame()
-        
     }else{
         alert('You already have started the game')
     }
