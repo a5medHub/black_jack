3a6506287db09da864b9881f8156b03a4fde4429
A.Yassine <ayassine.auce@gmail.com>
2023-08-16 21:14:55 +0300

    make function newgame works

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,15 @@ let player = { //shorthand
     name:"ahmad",
     chips:200
 }
+let isAlive = false
 // let cards = []
 let sum = 0
 const messageEl = document.getElementById("message-el")
 const cardsEl = document.getElementById("cards-el")
 const sumEl = document.getElementById("sum-el")
 const playerEl = document.getElementById("player-el")
+
+messageEl.innerHTML = "Want to play a round?"
 cardsEl.innerHTML = "Cards: "
 sumEl.innerHTML = "Sum: "
 playerEl.innerHTML = `${player.name}: $${player.chips}`
@@ -20,9 +23,17 @@ function getRandomCard() {
 }
 
 function startGame() {
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    sum = firstCard + secondCard
-    cardsEl.innerHTML = `Cards: ${firstCard} ${secondCard}`
-    sumEl.innerHTML = `Sum: ${sum}`
+    if(isAlive == false){
+        isAlive = true
+        player.chips -= 50
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        sum = firstCard + secondCard
+        cardsEl.innerHTML = `Cards: ${firstCard} ${secondCard}`
+        sumEl.innerHTML = `Sum: ${sum}`
+        playerEl.innerHTML = `${player.name}: $${player.chips}`
+        
+    }else{
+        alert('You already have started the game')
+    }
 }
